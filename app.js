817131da1b0224ddedbf4f3074a8fd87dd639f4c
QAISER ALI <qaisers.official@gmail.com>
2025-03-04 22:55:00 +0500

    Add session management and authentication middleware with flash messages

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000;
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
+const expressSession = require('express-session');
+const flash = require('connect-flash');
+
 const usersRouter = require('./routes/usersRouter');
 const productsRouter = require('./routes/productsRouter');
 const ownersRouter = require('./routes/ownersRouter');
@@ -12,6 +15,13 @@ const ownersRouter = require('./routes/ownersRouter');
 require('dotenv').config();
 
 const db = require('./config/mongoose-connection');
+
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+}));
+app.use(flash());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
