df404831f7a999e214124ba0d5cff26480f16bf5
QAISER ALI <qaisers.official@gmail.com>
2025-03-04 21:40:10 +0500

    Add dotenv package and implement user registration route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require("../models/user-model");
 
 router.get('/', (req, res) => {
     res.send('user route');
+});
+// localhost:300/users/register
+router.get('/register',async (req, res) => {
+   try{
+    let {fullname, email, password} = req.body;
+
+   const user =  await userModel.create({
+        fullname,
+        email,
+        password
+    });
+    res.send(user);
+   }catch(err){
+    res.send(err.message);
+   }
+    
 });
 
 module.exports = router;
