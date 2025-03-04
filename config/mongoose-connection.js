c4c4bc7791db8bb33bd7d1304c5bc0b5075e9731
QAISER ALI <qaisers.official@gmail.com>
2025-03-04 07:47:56 +0500

    Fix mongoose connection string formatting

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -4,7 +4,7 @@ const dbgr = require('debug')('app:mongoose-connection');
 
 const config = require('config');
 
-mongoose.connect(` ${config.get("MONGODB_URI")}/E-Commerce`).then(() => {
+mongoose.connect(`${config.get("MONGODB_URI")}/E-Commerce`).then(() => {
     dbgr('Database connected');
 }).catch((err) => {
     dbgr(err);
